2f1dfc27ae44ec5a5f6f57238ad482fd6b3c15dd
kierandelacruz-solirius <kieran.delacruz@solirius.com>
2026-02-09 17:09:44 +0000

    Kieran Local Ver (Most Recent)
    Mon 9 Feb, 5:02PM

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,68 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post('/casework-area-answer', function (req, res) {
+
+
+  var caseworkarea = req.session.data['casework-area']
+  if (caseworkarea == "Planning, Environmental and Applications") {
+    res.redirect('/cases/create-a-case/questions/peas-type-of-case')
+  } else if (caseworkarea == "Rights of Way and Common Land") {
+
+    res.redirect('/cases/create-a-case/questions/row-type-of-case') 
+  }
+
+})
+
+router.post('/peas-type-of-case-answer', function (req, res) {
+
+
+  var peastype = req.session.data['peas-type-of-case']
+  if (peastype == "Drought") {
+    res.redirect('/cases/create-a-case/questions/drought-subtype')
+
+  } else if (peastype == "Housing and Planning CPOs") {
+    res.redirect('/cases/create-a-case/questions/housing-planning-cpos-subtype') 
+
+  } else if (peastype == "Other Secretary of State casework") {
+    res.redirect('/cases/create-a-case/questions/other-sos-casework-subtype') 
+
+  } else if (peastype == "Purchase Notices") {
+    res.redirect('/cases/create-a-case/questions/case-name') 
+
+  } else if (peastype == "Wayleaves") {
+    res.redirect('/cases/create-a-case/questions/wayleaves-subtype') 
+  } 
+
+})
+
+router.post('/drought-subtype-answer', function (req, res) {
+
+
+  var droughtsub = req.session.data['drought-subtype']
+  if (droughtsub == "Drought Permits") {
+    res.redirect('/cases/create-a-case/questions/case-name')
+  } else if (droughtsub == "Drought Orders") {
+
+    res.redirect('/cases/create-a-case/questions/case-name') 
+  }
+
+})
+
+router.post('/row-type-of-case-answer', function (req, res) {
+
+
+  var rowtype = req.session.data['row-type-of-case']
+  if (rowtype == "Coastal Access") {
+    res.redirect('/cases/create-a-case/questions/coastal-subtype')
+
+  } else if (rowtype == "Common Land") {
+    res.redirect('/cases/create-a-case/questions/common-land-subtype') 
+
+  } else if (rowtype == "Rights of Way") {
+    res.redirect('/cases/create-a-case/questions/row-subtype') 
+}
+
+})
