e42bf4571648513ccc3a944bffc4aab7c715802c
kierandelacruz-solirius <kieran.delacruz@solirius.com>
2026-03-02 14:55:12 +0000

    Revert "installed express to allow moj js to be found and run on azure"
    This reverts commit 65265739ce8f1d33916a7921c4a2e363442c3365.

--- app/routes.js
+++ app/routes.js
@@ -6,12 +6,6 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-const express = require('express');
-const path = require('path');
-
-// --- AZURE BYPASS TUNNEL FOR MOJ ASSETS ---
-router.use('/safe-moj', express.static(path.join(__dirname, '../node_modules/@ministryofjustice/frontend')));
-
 
 // Page routes
 
